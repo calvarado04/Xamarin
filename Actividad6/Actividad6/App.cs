8ba92d5290c5ff1f34ffe0f381ead974aa5f03ff
Carlos Alvarado <carlosam@mx1.ibm.com>
2016-05-05 02:28:03 -0500

    Se actualiza actividad 6

--- Actividad6/Actividad6/App.cs
+++ Actividad6/Actividad6/App.cs
@@ -23,16 +23,10 @@ namespace Actividad6
 		{
 			MainPage = GetMainPage ();
 		}
-
-		public static INavigation Navigation { get; private set; }
-
-
-		public static Page GetMainPage() 
+			
+		//Ahora regresamos una pagina de tipo NavigationPage para navegar entre pantallas
+		public static NavigationPage GetMainPage ()
 		{	
-
-			var usuario = new Entry { Placeholder = "Usuario" };
-
-			var password = new Entry { Placeholder = "Password", IsPassword = true };
 			//Creamos un boton con fondo verde y texto blanco
 			Button btnLogin = new Button {
 				Text = "Login",
@@ -40,22 +34,26 @@ namespace Actividad6
 				BackgroundColor = Color.FromHex ("77D065")
 			};
 
-			ContentPage contentPage = new ContentPage();
 
+			var usuario = new Entry { Placeholder = "Usuario" };
+
+			var password = new Entry { Placeholder = "Password", IsPassword = true };
+
+			ContentPage contentPage = new ContentPage();
 			//Stacklayout permite agregar controles verticalmente
 			//Por el momento solo tenemos un control que es el boton
 			contentPage.Content = new StackLayout {
 				//Espacio alrededor de los controles
 				Padding = 10,
-				//Centramos el botón al centro
-				VerticalOptions = LayoutOptions.Center,
-				//Agregamos el botón
+
 				Children = {
 					new Label
 					{
-						Text = "Probar conexión a 104.42.52.205/mobile/login",
+						Text = "Probar conexión a Fedomex.xyz",
 						TextColor = Color.Silver,
-						FontSize = 22
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.StartAndExpand,
+						FontSize = 20
 					},
 					usuario,
 					password,
@@ -64,81 +62,39 @@ namespace Actividad6
 					{
 						Text = "Actividad 6 Carlos Alvarado Martínez",
 						TextColor = Color.Silver,
-						FontSize = 18
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.EndAndExpand,
+						FontSize = 20
 					}
 				}
 			};
 
-			//Cuando se le da clic al boton de Login, la aplicación se conectará a 104.42.52.205/mobile/login
-			//Pasando como argumento de usuario y contraseña lo que se haya introducido
-			//Se obtiene la cadena de respuesta del servidor, que se comparará para determinar
-			//si la conexión fue aceptada o fue rechazada por el servidor
 			btnLogin.Clicked += async (object sender, EventArgs e) => {
 
 				//URL de la página de login de Fedomex
-				string url = @"http://104.42.52.205/mobile/login";
+				string url = "http://fedomex.xyz/Tienda-en-Linea/login.html";
 				string result = String.Empty;
 
 				//Se usa el HttpClient
-				using (var client = new HttpClient()) {
-					var content = new FormUrlEncodedContent(new[] {
-						new KeyValuePair<string, string>("user", usuario.Text),
-						new KeyValuePair<string, string>("password", password.Text)
+				using (var client = new HttpClient ()) {
+					var content = new FormUrlEncodedContent (new[] {
+						new KeyValuePair<string, string> ("username", usuario.Text),
+						new KeyValuePair<string, string> ("password", password.Text)
 					});
 
 					//Se espera la respuesta del servidor y se guarda en un string
-					using (var response = await client.PostAsync(url, content)) {
+					using (var response = await client.PostAsync (url, content)) {
 						using (var responseContent = response.Content) {
-							result = await responseContent.ReadAsStringAsync();
-
-							//Declaramos el statusobj que es un objeto de clase Status
-							Status statusobj = new Status();
-
-							//Poblamos el objeto result con el json obtenido de la página web
-							JsonConvert.PopulateObject(result, statusobj);
-
-							//Asignamos el método status del objeto result de tipo Status a la variable resultado
-							string resultado = statusobj.status;
+							result = await responseContent.ReadAsStringAsync ();
 
 							//IF Statement, si en la cadena que se obtuvo aparece Bienvenido de nuevo,
 							//Se cambia al nuevo StackLayout y si no, no regresa a la pantalla principal
-							if (resultado.Contains("ok")) {
+							if (result.Contains ("Bienvenido de nuevo")) {
 								result = "Sí";
 
-								//Botón de regreso en el nuevo StackLayout
-								Button btnBack = new Button {
-									Text = "Regresar",
-									TextColor = Color.White,
-									BackgroundColor = Color.FromHex ("77D065")
-								};
-
-								//Contenido del nuevo StackLayout
-								contentPage.Content = new StackLayout {
-									Padding = 4,
-									VerticalOptions = LayoutOptions.Center,
-									Children = {
-										new Label
-										{
-											Text = "¡La conexión a 104.42.52.205/mobile/login fue exitosa!",
-											TextColor = Color.Silver, 
-											FontSize = 22
-										},
-										btnBack,
-										new Label
-										{
-											Text = "Actividad 6 Carlos Alvarado Martínez",
-											TextColor = Color.Silver,
-											FontSize = 18
-										}
-									}
-								};
-
-								//Cuando el Botón de regreso se presiona, se regresa a la pantalla principal
-								btnBack.Clicked += (sender2, f) => {
-									contentPage.Navigation.PushModalAsync(GetMainPage ());;
-								};
-
-
+								//Generamos una nueva página de navegación NewPage
+								var todoPage = new NewPage ();
+								await contentPage.Navigation.PushAsync (todoPage);
 
 							}
 
@@ -150,7 +106,7 @@ namespace Actividad6
 					}
 
 					//Muestra el display Alert requerido, tanto para sí o para no	
-					await contentPage.DisplayAlert("Respuesta del servidor",result, "OK");
+					await contentPage.DisplayAlert ("Respuesta del servidor", result, "OK");
 				}
 
 			};
---
+++ Actividad6/Actividad6/NewPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Actividad6
+{
+	public class NewPage : ContentPage
+	{
+
+		public NewPage ()
+		{
+
+			Content = new StackLayout { 
+
+				Children = {
+					new Label
+					{
+						Text = "¡La conexión a Fedomex.xyz fue exitosa!",
+						TextColor = Color.Silver,
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.StartAndExpand,
+						FontSize = 20
+					},
+					new Label
+					{
+						Text = "Esta es una nueva página de navegación",
+						TextColor = Color.Silver,
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.CenterAndExpand,
+						FontSize = 20
+					},
+					new Label
+					{
+						Text = "Actividad 6 Carlos Alvarado Martínez",
+						TextColor = Color.Silver,
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.EndAndExpand,
+						FontSize = 20
+					}
+				}
+			};
+		}
+	}
+}
